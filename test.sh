bc345b930fe7b4ab047d0bf4185aa73c0a407924
arch1987 <agajula028@gmail.com>
2020-06-11 12:37:26 +0530

    adding guru99 project

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Test the JMeter Docker image using a trivial test plan.
+
+# Example for using User Defined Variables with JMeter
+# These will be substituted in JMX test script
+# See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
+export TARGET_HOST="guru99.com"
+export TARGET_PATH="/software-testing.html"
+
+
+T_DIR=tests
+R_DIR=reports
+
+./run.sh  -JTARGET_HOST=${TARGET_HOST} \ -JTARGET_PATH=${TARGET_PATH} \ -n -t ${T_DIR}/Test-Plan.jmx -l ${T_DIR}/test-plan.jtl \ -e -o ${R_DIR}
+
+echo "==== Raw Test Report ===="
+cat ${T_DIR}/test-plan.jtl
+
+echo "==== HTML Test Report ===="
+echo "See HTML test report in ${R_DIR}/index.html"
