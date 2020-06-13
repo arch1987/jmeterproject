ff4f69433bb6d4dd4ba15f17b75e3adff5569ef6
arch1987 <agajula028@gmail.com>
2020-06-13 18:07:36 +0530

    adding lifecharger.jmx file

--- test.sh
+++ test.sh
@@ -5,13 +5,14 @@
 # Example for using User Defined Variables with JMeter
 # These will be substituted in JMX test script
 # See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
-export TARGET_HOST="guru99.com"
-export TARGET_PATH="/software-testing.html"
+export TARGET_HOST="lifecharger.org"
+export TARGET_PATH="/archives/"
+export TARGET_PATH1="/2-minute-rule/"
 
 T_DIR=tests
 R_DIR=T_DIR
 
-./run.sh  -JTARGET_HOST=${TARGET_HOST} \ -JTARGET_PATH=${TARGET_PATH} \ -n -t ${T_DIR}/Test-Plan1.jmx -l ${T_DIR}/test-plan.jtl \ -e -o ${R_DIR}
+./run.sh  -JTARGET_HOST=${TARGET_HOST} \ -JTARGET_PATH=${TARGET_PATH} \ -JTARGET_PATH=${TARGET_HOST}${TARGET_PATH1} \ -n -t ${T_DIR}/lifecharger_testplan.jmx -l ${T_DIR}/test-plan.jtl \ -e -o ${R_DIR}
 
 echo "==== Raw Test Report ===="
 cat ${T_DIR}/test-plan.jtl
