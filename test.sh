a2eb60a880bc33b339918a6d74b47b2f75c0d64e
arch1987 <agajula028@gmail.com>
2020-06-13 18:21:54 +0530

    changes in path test.sh

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ export TARGET_PATH1="/2-minute-rule/"
 T_DIR=tests
 R_DIR=T_DIR
 
-./run.sh  -JTARGET_HOST=${TARGET_HOST} \ -JTARGET_PATH=${TARGET_PATH} \ -JTARGET_PATH=${TARGET_HOST}${TARGET_PATH1} \ -n -t ${T_DIR}/lifecharger_testplan.jmx -l ${T_DIR}/test-plan.jtl \ -e -o ${R_DIR}
+./run.sh  -JTARGET_HOST=${TARGET_HOST} \ -JTARGET_PATH=${TARGET_HOST}${TARGET_PATH} \ -JTARGET_PATH=${TARGET_HOST}${TARGET_PATH1} \ -n -t ${T_DIR}/lifecharger_testplan.jmx -l ${T_DIR}/test-plan.jtl \ -e -o ${R_DIR}
 
 echo "==== Raw Test Report ===="
 cat ${T_DIR}/test-plan.jtl
